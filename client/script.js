d9918b17c1aa1923684cf1626e0581bb12811d73
Harris Arshad <harisarshad235@gmail.com>
2023-01-27 20:47:08 +0500

    changes

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codex-t4pg.onrender.com/', {
+    const response = await fetch('https://codex-your-d3inkoeb9-harisarshad235.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
